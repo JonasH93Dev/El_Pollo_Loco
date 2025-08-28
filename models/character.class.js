c6806540fdb2c7d286b6dbc0ffb91f6700cbb18c
JonasH93Dev <jonas.hanke@gmx.net>
2025-08-28 12:53:33 +0200

    feat: movable clouds

---
+++ models/character.class.js
@@ -0,0 +1,11 @@
+class Character extends MovableObject {
+
+    height = 280;
+    y = 155
+    width = 120;
+    constructor() {
+        super().loadImage('./assets/img/2_character_pepe/2_walk/W-21.png');
+    }
+    jump() {
+    }
+}
---
+++ models/world.class.js
@@ -0,0 +1,56 @@
+class World {
+
+    character = new Character();
+    enemies = [
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+    ];
+    clouds = [
+        new Cloud(),
+    ];
+    backgroundobjects = [
+        new BackgroundObject('./assets/img/5_background/layers/air.png', 0),
+        new BackgroundObject('./assets/img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('./assets/img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('./assets/img/5_background/layers/1_first_layer/1.png', 0)
+        
+    ];
+
+    canvas;
+    ctx;
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
+        this.draw();
+    }
+
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.addObjectsToMap(this.backgroundobjects);
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.clouds);
+
+
+
+
+        let self = this;
+        requestAnimationFrame(function () {
+            self.draw();
+        });
+
+    }
+
+    addObjectsToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o);
+        });
+    }
+
+    addToMap(movableObject) {
+        this.ctx.drawImage(movableObject.img, movableObject.x, movableObject.y, movableObject.width, movableObject.height);
+    }
+}
