559a887c6e3cae939c0189cb30fa2768163fe747
JonasH93Dev <jonas.hanke@gmx.net>
2025-09-01 09:18:08 +0200

    feat: move camera

--- models/world.class.js
+++ models/world.class.js
@@ -20,6 +20,7 @@ class World {
     canvas;
     ctx;
     keyboard;
+    camera_x = 0;
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -37,12 +38,15 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+
+        this.ctx.translate(this.camera_x, 0); 
+
         this.addObjectsToMap(this.backgroundobjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.enemies);
         this.addObjectsToMap(this.clouds);
 
-
+        this.ctx.translate(-this.camera_x, 0 ); 
 
 
         let self = this;
